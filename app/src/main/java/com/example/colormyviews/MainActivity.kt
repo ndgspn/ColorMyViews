dc916e0499d8c289d7f48b618a0415326fb42843
Nan <nandangsopyan08@gmail.com>
2020-06-25 13:53:49 +0700

    updated

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.colormyviews
 
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
